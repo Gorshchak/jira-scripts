d7399ea1deec59177a0c887d2ca58621486b57db
Gorshchak <77955590+Gorshchak@users.noreply.github.com>
2024-07-03 14:00:43 +0300

    Update custom-script.js

--- custom-script.js
+++ custom-script.js
@@ -1,26 +1,35 @@
 (function($){
     $(document).ready(function(){
+        console.log("Script loaded and document ready"); // Отладка
+
         var target = $('.ghx-columns');
-
-        var observer = new MutationObserver(function(mutations) {
-            mutations.forEach(function(mutation) {
-                if (mutation.type === 'childList' && mutation.addedNodes.length) {
-                    var columns = $('.ghx-column');
-                    columns.each(function() {
-                        var column = $(this);
-                        column.on('dragover', function() {
-                            column.css('background-color', 'rgba(0, 255, 0, 0.3)'); // Измените цвет по вашему усмотрению
-                        });
-                        column.on('dragleave', function() {
-                            column.css('background-color', ''); // Вернуть первоначальный цвет
-                        });
-                    });
-                }
-            });
-        });
-
         if (target.length) {
+            console.log("Target found"); // Отладка
+
+            var observer = new MutationObserver(function(mutations) {
+                mutations.forEach(function(mutation) {
+                    if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                        console.log("Child nodes added"); // Отладка
+
+                        var columns = $('.ghx-column');
+                        columns.each(function() {
+                            var column = $(this);
+                            column.on('dragover', function() {
+                                console.log("Drag over column"); // Отладка
+                                column.css('background-color', 'rgba(0, 255, 0, 0.3)'); // Измените цвет по вашему усмотрению
+                            });
+                            column.on('dragleave', function() {
+                                console.log("Drag leave column"); // Отладка
+                                column.css('background-color', ''); // Вернуть первоначальный цвет
+                            });
+                        });
+                    }
+                });
+            });
+
             observer.observe(target[0], { childList: true, subtree: true });
+        } else {
+            console.log("Target not found"); // Отладка
         }
     });
 })(AJS.$);
